867fa81b477aaa3e93871a1be0087e1ebd302c90
yangyao <ejoful@gmail.com>
2015-09-04 13:23:01 +0800

    3.5

---
+++ 3_5/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str1, str2, str3, str4;
+    cin >> str1 >> str2 >> str3;
+    str4 = str1 + str2 + str3;
+    cout << str4 << endl;
+    return 0;
+}
