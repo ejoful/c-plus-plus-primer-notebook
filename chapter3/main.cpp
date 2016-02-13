5f378db0fe214ce7574bfc0810a2f77cc6afca02
ejoful <ejoful@gmail.com>
2016-02-13 12:24:05 +0800

    123

--- chapter3/main.cpp
+++ chapter3/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string num;
+    vector<string> ivec;
+    while (cin >> num)
+    {
+            ivec.push_back(num);
+
+    }
+   for(int i = 0; i < ivec.size(); ++i)
+             cout<<ivec[i]<<endl;
     return 0;
 }
